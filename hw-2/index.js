123f8f0732f4b4a02dc2553666fe03aec964de41
Lena <lambitska92@gmail.com>
2021-11-06 11:30:06 +0200

    Final hw-2

---
+++ hw-2/index.js
@@ -0,0 +1,26 @@
+let n;
+while (true) {
+    n = prompt('Число для подсчета обобщенного числа Фибоначчи', '5');
+    if (isNaN(n) || !Number.isInteger(+n)) {
+        alert ('Пожалуйста, введите целое число!')
+    } else {
+        break;
+    }
+}
+function fibonacci (F0, F1, n) {
+    if (n > 0) {
+        for (let i = 0; i < n; i++) {
+            let temp = F1;
+            F1 = F0 + F1;
+            F0 = temp;
+        }
+    } else if (n < 0) {
+        for (let i = n; i < 0; i++) {
+            let temp = F0;
+            F0 = F1 - F0;
+            F1 = temp;
+        }
+    }
+    return F0;
+}
+alert (`Обобщенное число Фибоначчи: ${fibonacci (0,1, n)}`);
